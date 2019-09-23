252dc0ac27847dd233cd98d9deabcba9119eac90
Alexey Rogachevskiy <sfalexrog@gmail.com>
2019-09-23 14:03:39 +0300

    gles_undistort: Create EGL context in callback
    Looks like onInit and image callbacks get called on different threads. That's inconvenient to say the least, and might break EGL altogether, but that's what we have to work with.
    Currently we just output a debug image and ignore everything else. This will change in a future commit.

--- gles_undistort/src/gles_undistort.cpp
+++ gles_undistort/src/gles_undistort.cpp
@@ -2,12 +2,24 @@
 #include <ros/ros.h>
 #include <pluginlib/class_list_macros.h>
 #include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 #include <EGL/egl.h>
 #include <GLES3/gl3.h>
 #include <functional>
+#include <iostream>
 
 // DANGER: OpenGL (ES) code ahead!
+
+// Allow skipping pbuffer creation
+#ifndef CREATE_PBUFFER
+#define CREATE_PBUFFER 0
+#endif /* CREATE_PBUFFER */
+
+#define _XSTR(s) _STR(s)
+#define _STR(s) #s
+
+#define OGL_CHECKED(func) {GLenum error = glGetError(); if (error != GL_NO_ERROR) std::cerr << "OpenGL error at " << std::dec << __LINE__ << "while running" << _STR(func) << ": 0x" << std::hex << error << std::endl;}
 
 namespace
 {
@@ -26,8 +38,8 @@ private:
     {
         EGLDisplay display = EGL_NO_DISPLAY;
         EGLConfig config[MAX_NUM_CONFIG];
-        EGLContext context;
-        EGLSurface surface;
+        EGLContext context = EGL_NO_CONTEXT;
+        EGLSurface surface = EGL_NO_SURFACE;
         EGLint numConfig;
         EGLint major, minor;
     } eglConnectionState;
@@ -90,10 +102,29 @@ private:
             NODELET_ERROR("Could not create context; error %x", error);
             return;
         }
+#if CREATE_PBUFFER
+        EGLint pbufferAttribs[] = {
+            EGL_WIDTH, 640,
+            EGL_HEIGHT, 480,
+            EGL_NONE
+        };
+        eglConnectionState.surface = eglCreatePbufferSurface(eglConnectionState.display, eglConnectionState.config[0], pbufferAttribs);
+        if (eglConnectionState.surface == EGL_NO_SURFACE)
+        {
+            EGLint error = eglGetError();
+            NODELET_ERROR("Could not create surface; error %x", error);
+        }
+#endif
         // FIXME: check whether eglMakeCurrent succeeded?
-        eglMakeCurrent(eglConnectionState.display, EGL_NO_SURFACE, EGL_NO_SURFACE, eglConnectionState.context);
+        result = eglMakeCurrent(eglConnectionState.display, eglConnectionState.surface, eglConnectionState.surface, eglConnectionState.context);
+        if (result != EGL_TRUE)
+        {
+            EGLint error = eglGetError();
+            NODELET_ERROR("Somehow we could not set the context as current; error %x", error);
+        }
         // Perform a simple smoke test. This typically fails hard if no context is available or a context is bad for some reason
         NODELET_INFO("Created EGL context. Vendor: %s, renderer: %s, version: %s", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION));
+        NODELET_INFO("Context handle is %p", eglConnectionState.context);
     }
 
     struct FboData
@@ -106,10 +137,11 @@ private:
 
     FboData createFbo(GLsizei width, GLsizei height)
     {
-        FboData fbo;
+        FboData fbo{0, 0, 0, 0};
         glGenFramebuffers(1, &fbo.framebuffer);
         glBindFramebuffer(GL_FRAMEBUFFER, fbo.framebuffer);
         glGenTextures(1, &fbo.texture);
+        NODELET_INFO("Generated framebuffer %d and texture %d", fbo.framebuffer, fbo.texture);
         glBindTexture(GL_TEXTURE_2D, fbo.texture);
         fbo.width = width;
         fbo.height = height;
@@ -119,9 +151,16 @@ private:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo.texture, 0);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo.texture, 0); OGL_CHECKED(glFramebufferTexture2D);
+        glFramebufferTexture2D(GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fbo.texture, 0); OGL_CHECKED(glFramebufferTexture2D);
+        GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+        if (status != GL_FRAMEBUFFER_COMPLETE)
+        {
+            std::cerr << "Incomplete framebuffer, next operations will fail";
+        }
         glClearColor(0.0, 0.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
+        return fbo;
     }
 
     void deleteFbo(FboData& data)
@@ -136,6 +175,7 @@ private:
     FboData testFbo;
 
     image_transport::Publisher fboPub;
+    image_transport::CameraSubscriber camSub;
 
     ros::Timer timer_;
 public:
@@ -150,23 +190,54 @@ public:
         image_transport::ImageTransport it_(nh_);
         image_transport::ImageTransport it_priv_(nh_priv_);
 
-        createEglContext();
-        
-        testFbo = createFbo(640, 480);
+        //createEglContext();
 
         fboPub = it_priv_.advertise("image_rect", 1);
 
-        timer_ = nh_.createTimer(ros::Duration(10), std::bind(&Undistorter::publishFbo, this, std::placeholders::_1));
-
-
+        camSub = it_.subscribeCamera("image_raw", 1, &Undistorter::cameraCallback, this);
     }
 
-    void publishFbo(const ros::TimerEvent& event)
+    float colR, colG, colB;
+
+    void cameraCallback(const sensor_msgs::ImageConstPtr& src, const sensor_msgs::CameraInfoConstPtr& cameraInfo)
     {
+        if (eglGetCurrentContext() == nullptr)
+        {
+            NODELET_WARN("Recreating context!");
+            createEglContext();
+            testFbo = createFbo(src->width, src->height);
+        }
 
+        cv::Mat gl_image(testFbo.height, testFbo.width, CV_8UC4, cv::Scalar(0, 0, 255, 255));
+        colR = (ros::Time::now().nsec) / 1e9f;
+        colG = 1 - colR;
+        colB = colR * colG;
 
+        //NODELET_INFO("Publishing framebuffer %d, size (%d; %d)", testFbo.framebuffer, testFbo.width, testFbo.height);
+        EGLContext ctx = eglGetCurrentContext();
+        //NODELET_INFO("Using context handle %lx", ctx);
+
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, testFbo.framebuffer);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, testFbo.framebuffer);
+        glViewport(0, 0, testFbo.width, testFbo.height);
+        glClearColor(colR, colG, colB, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT); OGL_CHECKED(glClear);
+        glPixelStorei(GL_PACK_ALIGNMENT, 4);
+        glPixelStorei(GL_PACK_ROW_LENGTH, gl_image.step / gl_image.elemSize());
+        glReadBuffer(GL_COLOR_ATTACHMENT0);
+        
+        glFlush();
+        glFinish();
+        glReadPixels(0, 0, testFbo.width, testFbo.height, GL_RGBA, GL_UNSIGNED_BYTE, gl_image.ptr()); OGL_CHECKED(glReadPixels);
+
+        cv_bridge::CvImage img;
+        img.image = gl_image;
+        img.encoding = "bgra8";
+        img.header.frame_id = "body";
+        img.header.stamp = ros::Time::now();
+
+        fboPub.publish(img.toImageMsg());
     }
-
 };
 
 }
