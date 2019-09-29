40041896412238c33747f915b48f5ae5adb47f04
Oleg Kalachev <okalachev@gmail.com>
2019-09-29 14:18:59 +0200

    Update flight script

--- imav/src/flight.py
+++ imav/src/flight.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import rospy
 from clever import srv
 from std_srvs.srv import Trigger
@@ -24,4 +25,28 @@ def qr_cb(msg):
 qr_sub = rospy.Subscriber('qr_reader/qr', DetectedQr, qr_cb, queue_size=1,)
 
 
-rospy.spin()
+def scan():
+    print navigate(x=0, y=0, z=0.7, speed=0.5, frame_id='body', auto_arm=True)
+    rospy.sleep(5)
+    print navigate(x=0, y=4, z=0, speed=0.5, frame_id='navigate_target')
+    rospy.sleep(10)
+    print navigate(x=0, y=-2, z=0.4, speed=0.5, frame_id='navigate_target')
+    rospy.sleep(10)
+    print navigate(x=2, y=0, z=0, speed=0.5, frame_id='navigate_target')
+    rospy.sleep(5)
+    land()
+
+
+def fly_to_shelf():
+    print navigate(x=0, y=0, z=0.5, speed=0.5, frame_id='body', auto_arm=True)
+    rospy.sleep(3)
+    print navigate(x=4, y=0, z=0, speed=0.5, frame_id='navigate_target')
+    rospy.sleep(10)
+    print navigate(x=0, y=0, z=0, yaw=math.pi, speed=0.5, frame_id='navigate_target')
+    rospy.sleep(3)
+    print navigate(x=0, y=7.5, z=0, speed=0.5, frame_id='navigate_target')
+
+
+fly_to_shelf()
+
+# rospy.spin()
