c1447aff5e74508dcbca49827273388f331f5599
Oleg Kalachev <okalachev@gmail.com>
2019-09-30 12:53:35 +0200

    Update flag detector

--- flag_detector/cvTemplates/main.py
+++ flag_detector/cvTemplates/main.py
@@ -7,8 +7,10 @@ import glob
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from flag_detector.mag import Flag
 
 image_pub = rospy.Publisher('/line_detection/image', Image, queue_size=1)
+flag_message = rospy.Publisher('/detected_flag', Flag, queue_size=1)
 
 rospy.init_node('flag_detector')
 
@@ -19,11 +21,12 @@ bridge = CvBridge()
 
 
 images = []
+w, h = 115, 73
 
 flags = glob.glob("flags/*.png")
 for flag in flags:
     images.append({
-        'img': cv.resize(cv.imread(flag), (53, 40)),
+        'img': cv.resize(cv.imread(flag), (w, h)),
         'name': flag
     })
 
@@ -35,7 +38,13 @@ def detect_image(image):
         res = cv.matchTemplate(img, flag['img'], method)
         loc = np.where(res >= threshold)
         for pt in zip(*loc[::-1]):
+            cv.rectangle(image, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)
             cv.putText(image, flag['name'], pt, cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0))
+            flags = Flag(
+                header=''
+                country=''
+            )
+            flag_message.publish()
             break
 
     return image
@@ -43,16 +52,16 @@ def detect_image(image):
 
 def image_callback(data):
     print('callback')
+    if image_pub.get_num_connections() == 0:
+        return
+    else
+        cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image
 
-    cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')  # OpenCV image
-
-    img = detect_image(cv_image)
-    image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
+        img = detect_image(cv_image)
+        image_pub.publish(bridge.cv2_to_imgmsg(img, 'bgr8'))
 
 
 if __name__ == "__main__":
     print("Subscribe start")
     image_sub = rospy.Subscriber('main_camera/image_raw', Image, image_callback, queue_size=1)
-
-    while not rospy.is_shutdown():
-        rospy.sleep(0.2)
+    rospy.spin()
