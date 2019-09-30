0dd87a40c8390d3e219d253995308c206f021e3a
Alexey Rogachevskiy <sfalexrog@gmail.com>
2019-09-30 16:58:55 +0300

    text_detector: Add Tesseract OCR

--- text_detector/src/text_detector_nodelet.cpp
+++ text_detector/src/text_detector_nodelet.cpp
@@ -8,6 +8,9 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
 
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
 #include <functional>
 
 
@@ -65,56 +68,90 @@ private:
         }
     }
 
-std::vector<cv::String> outNames{"feature_fusion/Conv_7/Sigmoid", "feature_fusion/concat_3"};
+    std::vector<cv::String> outNames{"feature_fusion/Conv_7/Sigmoid", "feature_fusion/concat_3"};
 
-void detectionCallback(const sensor_msgs::ImageConstPtr &image)
-{
-    std::vector<cv::Mat> outs(2);
-    bool hasSubscribers = (debug_pub_.getNumSubscribers() > 0);
-    NODELET_INFO("Finding text in image");
-    cv_bridge::CvImageConstPtr sourceImg = cv_bridge::toCvShare(image);
-    cv::Mat blob;
-    NODELET_INFO("Generating input from image");
-    cv::dnn::blobFromImage(sourceImg->image, blob, 1.0, cv::Size(inputWidth_, inputHeight_), cv::Scalar(123.68, 116.78, 103.94), true, false);
-    NODELET_INFO("Setting up input for model");
-    model_.setInput(blob);
-    NODELET_INFO("Setting up outputs and their names");
-    model_.forward(outs, outNames);
-
-    cv::Mat scores = outs[0];
-    cv::Mat geometry = outs[1];
-
-    std::vector<cv::RotatedRect> boxes;
-    std::vector<float> confidences;
-    NODELET_INFO("Starting to decode outputs");
-    decode(scores, geometry, confThreshold_, boxes, confidences);
-
-    std::vector<int> indices;
-    cv::dnn::NMSBoxes(boxes, confidences, confThreshold_, nmsThreshold_, indices);
-    NODELET_INFO("Found %ld potential texts", indices.size());
-    if (debug_pub_.getNumSubscribers() > 0)
+    void detectionCallback(const sensor_msgs::ImageConstPtr &image)
     {
-        cv_bridge::CvImagePtr debugImg = cv_bridge::toCvCopy(image);
-        // Render detections.
-        NODELET_INFO("Publishing debug image");
+        std::vector<cv::Mat> outs(2);
+        bool hasSubscribers = (debug_pub_.getNumSubscribers() > 0);
+        NODELET_INFO("Finding text in image");
+        cv_bridge::CvImageConstPtr sourceImg = cv_bridge::toCvShare(image);
+        cv::Mat blob;
+        NODELET_INFO("Generating input from image");
+        cv::dnn::blobFromImage(sourceImg->image, blob, 1.0, cv::Size(inputWidth_, inputHeight_), cv::Scalar(123.68, 116.78, 103.94), true, false);
+        NODELET_INFO("Setting up input for model");
+        model_.setInput(blob);
+        NODELET_INFO("Setting up outputs and their names");
+        model_.forward(outs, outNames);
+
+        cv::Mat scores = outs[0];
+        cv::Mat geometry = outs[1];
+
+        std::vector<cv::RotatedRect> boxes;
+        std::vector<float> confidences;
+        NODELET_INFO("Starting to decode outputs");
+        decode(scores, geometry, confThreshold_, boxes, confidences);
+
+        std::vector<int> indices;
+        cv::dnn::NMSBoxes(boxes, confidences, confThreshold_, nmsThreshold_, indices);
         cv::Point2f ratio((float)sourceImg->image.cols / inputWidth_, (float)sourceImg->image.rows / inputHeight_);
-        for (size_t i = 0; i < indices.size(); ++i)
+        for(size_t i = 0; i < indices.size(); ++i)
         {
             cv::RotatedRect& box = boxes[indices[i]];
-
-            cv::Point2f vertices[4];
-            box.points(vertices);
-            for (int j = 0; j < 4; ++j)
-            {
-                vertices[j].x *= ratio.x;
-                vertices[j].y *= ratio.y;
+            cv::Mat rotMatrix, rotated, cropped;
+            float angle = box.angle;
+            cv::Size rectSize = box.size;
+            if (box.angle < -45.0) {
+                angle += 90.0;
+                cv::swap(rectSize.width, rectSize.height);
             }
-            for (int j = 0; j < 4; ++j)
-            cv::line(debugImg->image, vertices[j], vertices[(j + 1) % 4], cv::Scalar(0, 255, 0), 1);
+            rotMatrix = cv::getRotationMatrix2D(box.center, angle, 1.0);
+
+            cv::Mat boxROI = sourceImg->image(box.boundingRect());
+            cv::warpAffine(boxROI, rotated, rotMatrix, boxROI.size(), cv::INTER_CUBIC);
+            cv::getRectSubPix(rotated, rectSize, box.center, cropped);
+
+            cv::Mat source = sourceImg->image;
+            //tess_api_->TesseractRect(boxROI.data, boxROI.elemSize(), boxROI.step1(), 0, 0, boxROI.size().height, boxROI.size().width);
+            cv::Rect br = box.boundingRect();
+            br.x *= ratio.x;
+            br.y *= ratio.y;
+            br.width *= ratio.x;
+            br.height *= ratio.y;
+            NODELET_INFO("Bounding rect: (%d, %d, %d, %d)", br.x, br.y, br.width, br.height);
+            tess_api_->TesseractRect(source.data, source.elemSize(), source.step1(), br.x, br.y, br.width, br.height);
+            
+            const char *text = tess_api_->GetUTF8Text();
+            NODELET_INFO("Detected text for box %d: %s", i, text);
+            delete[] text;
         }
-        debug_pub_.publish(debugImg->toImageMsg());
+        NODELET_INFO("Found %ld potential texts", indices.size());
+        if (debug_pub_.getNumSubscribers() > 0)
+        {
+            cv_bridge::CvImagePtr debugImg = cv_bridge::toCvCopy(image);
+            // Render detections.
+            NODELET_INFO("Publishing debug image");
+            cv::Point2f ratio((float)sourceImg->image.cols / inputWidth_, (float)sourceImg->image.rows / inputHeight_);
+            for (size_t i = 0; i < indices.size(); ++i)
+            {
+                cv::RotatedRect& box = boxes[indices[i]];
+
+                cv::Point2f vertices[4];
+                box.points(vertices);
+                for (int j = 0; j < 4; ++j)
+                {
+                    vertices[j].x *= ratio.x;
+                    vertices[j].y *= ratio.y;
+                }
+                for (int j = 0; j < 4; ++j)
+                cv::line(debugImg->image, vertices[j], vertices[(j + 1) % 4], cv::Scalar(0, 255, 0), 1);
+            }
+            debug_pub_.publish(debugImg->toImageMsg());
+        }
     }
-}
+
+    std::shared_ptr<tesseract::TessBaseAPI> tess_api_;
+
 
 public:
     TextDetectorNodelet() {}
@@ -134,6 +171,15 @@ public:
         NODELET_INFO("Loading model: %s", modelName.c_str());
 
         model_ = cv::dnn::readNet(modelName);
+
+        NODELET_INFO("Initializing Tesseract API");
+        tess_api_ = std::make_shared<tesseract::TessBaseAPI>();
+
+        if (tess_api_->Init(NULL, "eng")) {
+            NODELET_ERROR("Could not initialize Tesseract API");
+        }
+
+        tess_api_->SetPageSegMode(tesseract::PageSegMode::PSM_SINGLE_LINE);
 
         NODELET_INFO("Subscribing to image topic");
 
