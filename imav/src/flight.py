213087f8c241bb52549905dc9d4d103b9bb95579
Oleg Kalachev <okalachev@gmail.com>
2019-09-30 12:43:58 +0200

    Updates to flight script

--- imav/src/flight.py
+++ imav/src/flight.py
@@ -22,23 +22,42 @@ set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
 land = rospy.ServiceProxy('land', Trigger)
 
 
-# ser = serial.Serial('usb-1a86_USB2.0-Serial-if00-port0', 115200, timeout=10)
-# servo_angle = 30
-# last_range = -1
+ser = serial.Serial('/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0', 115200, timeout=10)
+servo_angle = 30
+last_range = -1
 
 
-# def _arduino_comms():
-#     global last_range, servo_angle
-#     while True:
-#         last_range = int(ser.readline())
-#         ser.write(str(servo_angle))
+def _arduino_comms():
+    global last_range, servo_angle
+    while True:
+        last_range = float(ser.readline())
+        ser.write(str(servo_angle) + '\n')
 
 
-# thread.start_new_thread(_arduino_comms, [])
+def handle(pos):
+    global servo_angle
+    if pos == 'down':
+        servo_angle = 100
+    elif pos == 'keep':
+        servo_angle = 80
+    elif pos == 'drop':
+        servo_angle = 175
+    elif pos == 'back':
+        servo_angle = 30
+    else:
+        raise Exception('incorrect position')
+
+
+thread.start_new_thread(_arduino_comms, ())
+
+packages = []
 
 
 def qr_cb(msg):
-    print 'qr', msg
+    if msg.qr_message in packages:
+        print '!! found package', msg.qr_message
+    else:
+        print 'skip package', msg.qr_message
 
 
 qr_sub = rospy.Subscriber('qr_reader/qr', DetectedQr, qr_cb, queue_size=1,)
@@ -55,12 +74,19 @@ def navigate_wait(x=0, y=0, z=0, speed=0.5, frame_id='', tolerance=0.2, auto_arm
         if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
             print 'navigating finished'
             break
-        if timeout and rospy.get_rostime() - start > timeout:
+        if timeout and ((rospy.get_rostime() - start) > timeout):
             print 'navigating timeout'
             break
         rospy.sleep(0.2)
 
     return res
+
+
+def read_csv():
+    import csv
+    with open('packages.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        return list(row[0] for row in reader)
 
 
 def takeoff():
@@ -69,27 +95,25 @@ def takeoff():
 
 
 def scan():
-    print 'scan'
-    navigate(x=0, y=4, z=0, speed=0.4, frame_id='navigate_target')
-    rospy.sleep(10)
-    print 'scan back'
-    navigate(x=0, y=-2, z=0.4, speed=0.4, frame_id='navigate_target')
-    rospy.sleep(10)
-    print 'fly to window'
-    navigate(x=2, y=0, z=0, speed=0.5, frame_id='navigate_target')
-    rospy.sleep(7)
-    print 'land'
-    land()
+    print 'start scanning'
+    print 'go left'
+    navigate_wait(x=0, y=4.5, z=0, speed=0.3, frame_id='navigate_target')
+    print 'go up'
+    navigate_wait(x=0, y=0, z=2, speed=0.3, frame_id='navigate_target')
+    print 'go right'
+    navigate_wait(x=0, y=-4.5, z=0, speed=0.3, frame_id='navigate_target')
+    wait_aruco(120)
+    navigate_wait(x=0, y=0, z=2.5, speed=0.3, frame_id='aruco_120')
+    print 'fly through window'
+    navigate_wait(x=3, y=0, z=0, speed=0.5, frame_id='navigate_target')
 
 
 def fly_to_shelf():
-    print navigate(x=0, y=0, z=0.5, speed=0.5, frame_id='body', auto_arm=True)
+    print 'fly to shelf'
+    navigate_wait(x=4, y=0, z=0, speed=0.5, frame_id='navigate_target', timeout=rospy.Duration(8))
+    print 'adjust yaw'
+    navigate(x=0, y=0, z=0, yaw=math.pi, speed=0.5, frame_id='navigate_target')
     rospy.sleep(3)
-    print navigate(x=4, y=0, z=0, speed=0.5, frame_id='navigate_target')
-    rospy.sleep(10)
-    print navigate(x=0, y=0, z=0, yaw=math.pi, speed=0.5, frame_id='navigate_target')
-    rospy.sleep(3)
-    # print navigate(x=0, y=7.5, z=0, speed=0.5, frame_id='navigate_target')
 
 
 def wait_aruco(_id):
@@ -101,31 +125,55 @@ def wait_aruco(_id):
             break
         rospy.sleep(0.2)
 
-def mission():
-    print 'takeoff'
-    print navigate(x=0, y=0, z=1.5, speed=0.5, frame_id='body', auto_arm=True)
-    rospy.sleep(5)
 
-    print 'fly left'
-    navigate(x=0, y=5, z=0, speed=0.2, frame_id='navigate_target')
-
+def pick_payload():
+    handle('down')
     wait_aruco(3)
+    navigate_wait(x=0, y=0, z=1, speed=0.3, frame_id='aruco_3')
+    print 'backwards!'
+    navigate_wait(x=-1, speed=1, frame_id='navigate_target')
 
-    print 'fly to aruco'
-    navigate(x=0, y=0, z=1, speed=0.3, frame_id='aruco_3')
-    rospy.sleep(8)
+
+def land_to_target():
+    wait_aruco(3)
+    navigate_wait(x=0, y=0, z=1, speed=0.3, frame_id='aruco_3')
+    land()
+
+
+def mission():
+    handle('back')
+
+    print 'takeoff'
+    print navigate_wait(z=1.5, speed=1, frame_id='body', auto_arm=True, timeout=rospy.Duration(5))
+
+    fly_to_shelf()
+
+    print 'search for marker'
+    navigate(x=0, y=3, z=0, speed=0.2, frame_id='navigate_target')
+
+    wait_aruco(120)
+
+    print 'fly to aruco 120'
+    navigate_wait(x=0, y=0, z=1, speed=0.3, frame_id='aruco_120')
 
     print 'fly down'
-    navigate(x=0, y=0, z=-0.5, speed=0.3, frame_id='navigate_target')
-    rospy.sleep(4)
+    navigate_wait(x=0, y=0, z=-0.3, speed=0.3, frame_id='navigate_target', timeout=rospy.Duration(1))
 
     scan()
 
 
+packages = read_csv()
+print 'got packages', packages
 
 # print 'take off'
 # takeoff()
 mission()
+# print 'tkof'
+# print navigate(z=1.5, frame_id='body', auto_arm=True)
+# wait_aruco(0)
+# navigate(x=0, y=0, z=1, speed=0.3, frame_id='aruco_0')
+
+# print 'finish'
 
 #fly_to_shelf()
 
